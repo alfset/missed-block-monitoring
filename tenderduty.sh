14a6fc7340b0f8a0c0a795b2ca99c3e0f3023c23
Alfino Setiawan <42708476+alfset@users.noreply.github.com>
2023-01-19 12:37:25 +0700

    Create tenderduty.sh

---
+++ tenderduty.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Still Preparing"
+echo "=================================================="
+echo -e "\033[0;35m"
+echo " ░█████╗░░█████╗░███╗░░░███╗██╗░░░██╗███╗░░██╗██╗████████╗██╗░░░██╗░░░░░░███╗░░██╗░█████╗░██████╗░███████╗░██████╗";
+echo " ██╔══██╗██╔══██╗████╗░████║██║░░░██║████╗░██║██║╚══██╔══╝╚██╗░██╔╝░░░░░░████╗░██║██╔══██╗██╔══██╗██╔════╝██╔════╝";
+echo " ██║░░╚═╝██║░░██║██╔████╔██║██║░░░██║██╔██╗██║██║░░░██║░░░░╚████╔╝░█████╗██╔██╗██║██║░░██║██║░░██║█████╗░░╚█████╗░";
+echo " ██║░░██╗██║░░██║██║╚██╔╝██║██║░░░██║██║╚████║██║░░░██║░░░░░╚██╔╝░░╚════╝██║╚████║██║░░██║██║░░██║██╔══╝░░░╚═══██╗";
+echo " ╚█████╔╝╚█████╔╝██║░╚═╝░██║╚██████╔╝██║░╚███║██║░░░██║░░░░░░██║░░░░░░░░░██║░╚███║╚█████╔╝██████╔╝███████╗██████╔╝";
+echo " ░╚════╝░░╚════╝░╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚══╝╚═╝░░░╚═╝░░░░░░╚═╝░░░░░░░░░╚═╝░░╚══╝░╚════╝░╚═════╝░╚══════╝╚═════╝";
+echo -e "\e[0m"
+echo "=================================================="
+
+sleep 2
+#install depency
+sudo apt install docker -y
+sudo apt install wget -y
+mkdir tenderduty
+cd tenderduty
+docker run --rm ghcr.io/blockpane/tenderduty:latest -example-config >config.yml
+rm config.yml
+wget https://raw.githubusercontent.com/alfset/missed-block-monitoring/main/config.yml
+sleep 3
+##setting Variable
+valoperaddress= read -p 'valoper address: ' VALOPERADDRESS
+webhook= read -p 'webhook : ' WEBHOOK 
+echo "export VALOPERADDRESS=${VALOPERADDRESS}" >> $HOME/.bash_profile
+echo "export WEBHOOK=${WEBHOOK}" >> $HOME/.bash_profile
+sleep 3
+source $HOME/.bash_profile
+sed -i.back -e "s\webhook:*\webhook: \"$WEBHOOK\"\g" /workspace/missed-block-monitoring/tenderduty/config.yml
+sed -i.back -e "s/valoper_address:*/valoper_address: \"$VALOPERADDRESS\"/" /workspace/missed-block-monitoring/tenderduty/config.yml
+##run
+docker stop tenderduty
+docker rm tenderduty
+docker run -d --name tenderduty -p "8888:8888" -p "28686:28686" --restart unless-stopped -v $(pwd)/config.yml:/var/lib/tenderduty/config.yml ghcr.io/blockpane/tenderduty:latest
+docker logs -f --tail 20 tenderduty
+
